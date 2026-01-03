b6cc0e445252f7a989a0f73f4a67f7d9036f998e
Murali Nidugondi <muralinidugondi@Muralis-MacBook-Air.local>
2026-01-03 18:05:16 +0530

    Initial commit

---
+++ src/main/java/com/mn/springboot/person_service_api_mysql/PersonServiceApiMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.mn.springboot.person_service_api_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServiceApiMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServiceApiMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mn/springboot/person_service_api_mysql/PersonServiceApiMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mn.springboot.person_service_api_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApiMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
